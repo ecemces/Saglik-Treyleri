7f60d103563fe48391e38d9a21913809f4fa036c
ecemces <113671363+ecemces@users.noreply.github.com>
2022-12-13 16:57:16 +0300

    tema değiştirildi

--- serial-connect.py
+++ serial-connect.py
@@ -4,37 +4,39 @@ import serial
 import pyodbc
 import time
 import datetime
+import tkinter
 from datetime import date
 from datetime import datetime
+import sv_ttk
 today = date.today()
+login= tkinter.Tk()
+sv_ttk.set_theme("light") #lisans?
 
-login= tk.Tk()
-login['bg']="#b3daec"
-login.geometry('325x95')  
+login.geometry('460x145')  
 login.title('Giriş yap')
 login.iconbitmap("technopc-logo-png-6858-d.ico")
-usernameLabel = tk.Label(login, text="Kullanıcı adı",bg="#b3daec")
-usernameLabel.grid(row=0, column=0)
+usernameLabel = ttk.Label(login, text="Kullanıcı adı")
+usernameLabel.grid(row=0, column=0,pady=10,padx=10)
 username = tk.StringVar()
-usernameEntry = tk.Entry(login, width=40, textvariable=username)
-usernameEntry.grid(row=0, column=1)
-passwordLabel = tk.Label(login,text="Şifre",bg="#b3daec")
+usernameEntry = ttk.Entry(login, width=40, textvariable=username)
+usernameEntry.grid(row=0, column=1,pady=10,padx=10)
+passwordLabel = ttk.Label(login,text="Şifre")
 passwordLabel.grid(row=1, column=0) 
 password = tk.StringVar()
-passwordEntry = tk.Entry(login, textvariable=password, show='*',width=40)
+passwordEntry = ttk.Entry(login, textvariable=password, show='*',width=40)
 passwordEntry.grid(row=1, column=1)
-#kullanici_value= tk.StringVar()
+#kullanici_value= ttk.StringVar()
 #kullanici=kullanici_value.set(username.get)
 
 
 server = 'localhost' 
 database = 'SaglikTreyleri'
 cnxn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=localhost;'
+                      'Server=192.168.0.80;'
                       'Database=SaglikTreyleri;'
                       'Trusted_Connection=yes;')
 cursor = cnxn.cursor()
-exceptionWrong = tk.Label(login,text="      Kullanıcı adı ya da şifre yanlış",bg="#b3daec")
+exceptionWrong = ttk.Label(login,text="      Kullanıcı adı ya da şifre yanlış")
 
 def validateLogin(username, password):
     cursor.execute("SELECT * FROM kullanicilar")
@@ -48,33 +50,33 @@ def validateLogin(username, password):
             passwordEntry.grid_remove()
             loginButton.grid_remove()
             #window.deiconify()
-            button = tk.Button(login,text="1.çekmece ",width=27,height=4,fg="black",command=lambda:connect(username))
-            button.grid(row=0,column=0,sticky=tk.NS)
-            button2 = tk.Button(login,text="2.çekmece ",width=27, height=4,fg="black",command=lambda:connect2(username))
-            button2.grid(row=1,column=0,sticky=tk.NS)
-            button3 = tk.Button(login,text="3.çekmece ",width=27,height=4,fg="black",command=lambda:connect3(username))
-            button3.grid(row=2,column=0,sticky=tk.NS)
-            button4 = tk.Button(login,text="4.çekmece ",width=27,height=4,fg="black",command=lambda:connect4(username))
-            button4.grid(row=3,column=0,sticky=tk.NS)
-            button5 = tk.Button(login,text="5.çekmece ",width=27,height=4,fg="black",command=lambda:connect5(username))
-            button5.grid(row=4,column=0,sticky=tk.NS)
-            login.geometry('200x360')  #check
+            button = ttk.Button(login,text="1.çekmece ",command=lambda:connect(username))
+            button.grid(row=0,column=0,sticky=tk.NS,pady=16,padx=16)
+            button2 = ttk.Button(login,text="2.çekmece ",command=lambda:connect2(username))
+            button2.grid(row=1,column=0,sticky=tk.NS,pady=16,padx=16)
+            button3 = ttk.Button(login,text="3.çekmece ",command=lambda:connect3(username))
+            button3.grid(row=2,column=0,sticky=tk.NS,pady=16,padx=16)
+            button4 = ttk.Button(login,text="4.çekmece ",command=lambda:connect4(username))
+            button4.grid(row=3,column=0,sticky=tk.NS,pady=16,padx=16)
+            button5 = ttk.Button(login,text="5.çekmece ",command=lambda:connect5(username))
+            button5.grid(row=4,column=0,sticky=tk.NS,pady=16,padx=16)
+            login.geometry('130x310')  #check
             login.title('Çekmeceler')
              #exede açılacak mı bak
             break
         else:
-            login.geometry('325x130')
+            login.geometry('460x180')
             exceptionWrong.grid(row=5,column=1,rowspan=2)
             usernameEntry.delete(0, tk.END)
             passwordEntry.delete(0, tk.END)
     def connect(username): #fonksyionlar düzenlenebilir
             pump = serial.Serial(port='COM3', baudrate=115200)
             pump.write(b'from machine import Pin\r\n')
-            pump.write(b'cekmece1 = Pin(18, Pin.OUT)\r\n')
+            pump.write(b'cekmece1 = Pin(19, Pin.OUT)\r\n')
             pump.write(b'cekmece1.value(1)\r\n')
             time.sleep(1)
             pump.write(b'cekmece1.value(0)\r\n')
-            time.sleep(1)
+            time.sleep(3)
             pump.write(b'cekmece1.value(1)\r\n')
             now = datetime.now()
             tarih= today.strftime("%Y/%m/%d")
@@ -85,11 +87,11 @@ def validateLogin(username, password):
     def connect2(username):
             pump = serial.Serial(port='COM3', baudrate=115200)
             pump.write(b'from machine import Pin\r\n')
-            pump.write(b'cekmece2 = Pin(18, Pin.OUT)\r\n')
+            pump.write(b'cekmece2 = Pin(21, Pin.OUT)\r\n')
             pump.write(b'cekmece2.value(1)\r\n')
             time.sleep(1)
             pump.write(b'cekmece2.value(0)\r\n')
-            time.sleep(1)
+            time.sleep(3)
             pump.write(b'cekmece2.value(1)\r\n')
             now = datetime.now()
             tarih= today.strftime("%Y/%m/%d")
@@ -104,7 +106,7 @@ def validateLogin(username, password):
             pump.write(b'cekmece3.value(1)\r\n')
             time.sleep(1)
             pump.write(b'cekmece3.value(0)\r\n')
-            time.sleep(1)
+            time.sleep(3)
             pump.write(b'cekmece3.value(1)\r\n')
             now = datetime.now()
             tarih= today.strftime("%Y/%m/%d")
@@ -115,11 +117,11 @@ def validateLogin(username, password):
     def connect4(username):
             pump = serial.Serial(port='COM3', baudrate=115200)
             pump.write(b'from machine import Pin\r\n')
-            pump.write(b'cekmece4 = Pin(18, Pin.OUT)\r\n')
+            pump.write(b'cekmece4 = Pin(20, Pin.OUT)\r\n')
             pump.write(b'cekmece4.value(1)\r\n')
             time.sleep(1)
             pump.write(b'cekmece4.value(0)\r\n')
-            time.sleep(1)
+            time.sleep(3)
             pump.write(b'cekmece4.value(1)\r\n')
             now = datetime.now()
             tarih= today.strftime("%Y/%m/%d")
@@ -130,10 +132,10 @@ def validateLogin(username, password):
     def connect5(username):
             pump = serial.Serial(port='COM3', baudrate=115200)
             pump.write(b'from machine import Pin\r\n')
-            pump.write(b'cekmece5 = Pin(18, Pin.OUT)\r\n')
+            pump.write(b'cekmece5 = Pin(22, Pin.OUT)\r\n')
             pump.write(b'cekmece5.value(1)\r\n')
             time.sleep(1)
-            pump.write(b'cekmece5.value(0)\r\n')
+            pump.write(b'cekmece5.value(3)\r\n')
             time.sleep(1)
             pump.write(b'cekmece5.value(1)\r\n')
             now = datetime.now()
@@ -144,8 +146,9 @@ def validateLogin(username, password):
             cnxn.commit()
             #print(username)
             #return username            
-loginButton = tk.Button(login, text="Giriş yap", bg="#0083c0",fg="white",font='Helvetica 8 bold', command=lambda: validateLogin(username.get(),password.get()))
+loginButton = ttk.Button(login, text="Giriş yap", command=lambda: validateLogin(username.get(),password.get()))
 loginButton.grid(row=4, column=0,padx=15,pady=15,columnspan=2)
 login.mainloop()
+
 
 
