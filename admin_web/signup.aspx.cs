da218af97afd83ff62945945e6f6e96ff94aec2f
ecemces <113671363+ecemces@users.noreply.github.com>
2022-12-16 14:35:27 +0300

    Update signup.aspx.cs

--- admin_web/signup.aspx.cs
+++ admin_web/signup.aspx.cs
@@ -1,36 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
-
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Web.Helpers;
 namespace saglik_treyleri.web1
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
+       
         SqlConnection connect = new SqlConnection("Data Source=localhost;Initial Catalog=SaglikTreyleri;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         protected void submitbutton_Click(object sender, EventArgs e)
         {
+            
             connect.Open();
             string checkusername = "select * from [kullanicilar] where kullanici = @username ";
             SqlCommand command = new SqlCommand(checkusername, connect);
             command.Parameters.AddWithValue("@username", usernametxt.Text);
             SqlDataReader reader = command.ExecuteReader();
-
+            //admin ekleme özelliği yok
             if (!reader.HasRows && passwordtxt0.Text==passwordtxt.Text)
-            {
+            {   
                 reader.Close();
-                string createUser = "insert into [kullanicilar]  (kullanici, sifre) values(@kullanici, @sifre)";
+                var hash = Crypto.HashPassword(passwordtxt.Text);
+                string createUser = "insert into [kullanicilar]  (kullanici, sifre,yetki) values(@kullanici, @sifre,@yetki)";
                 SqlCommand addCommand = new SqlCommand(createUser, connect);
                 addCommand.Parameters.AddWithValue("@kullanici", usernametxt.Text);
-                addCommand.Parameters.AddWithValue("@sifre", passwordtxt.Text);
+                addCommand.Parameters.AddWithValue("@sifre", hash);
+                addCommand.Parameters.AddWithValue("@yetki", 0);
                 addCommand.ExecuteNonQuery();
                 connect.Close();
                 InvalidLabel.Visible = true;
@@ -52,6 +58,11 @@ namespace saglik_treyleri.web1
                 InvalidLabel.ForeColor = System.Drawing.Color.Red;
             }
 
+
+        }
+
+        protected void usernametxt_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
