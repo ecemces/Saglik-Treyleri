73bc8a15953168c295100f43c232e3d9416eb6bc
ecemces <113671363+ecemces@users.noreply.github.com>
2022-12-16 14:32:37 +0300

    Update login.aspx.cs

--- admin_web/login.aspx.cs
+++ admin_web/login.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,42 +7,41 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.IO;
 using System.IO.Ports;
-
+using System.Web.Helpers;
+using System.Text;
 namespace saglik_treyleri.web1
 {
     public partial class WebForm2 : System.Web.UI.Page
-    {
-        
-        SqlConnection connect = new SqlConnection("Data Source=localhost;Initial Catalog=SaglikTreyleri;Integrated Security=True");
-        
+    {        
+        SqlConnection connect = new SqlConnection("Data Source=localhost;Initial Catalog=SaglikTreyleri;Integrated Security=True");     
         protected void Page_Load(object sender, EventArgs e)
         {
         }
-
         protected void loginbutton_Click(object sender, EventArgs e)
         {
-           
-            string checkusername = "select * from [kullanicilar] where kullanici = @username and sifre = @password";
-            SqlCommand command = new SqlCommand(checkusername, connect);
             connect.Open();
+            string password = "select sifre from [kullanicilar] where kullanici = @username ";
+            SqlCommand command = new SqlCommand(password, connect);
             command.Parameters.AddWithValue("@username", usernametxt.Text);
-            command.Parameters.AddWithValue("@password", passwordtxt.Text);
-            SqlDataReader reader = command.ExecuteReader();
-
-            //if ((reader.HasRows) & (usernametxt.Text != "admin") & (passwordtxt.Text != "adminim"))
-            //{
-                
-                //reader.Close();
-                //connect.Close();
-                //Session["User"] = usernametxt.Text;
-                //Response.Redirect("anasayfa.aspx");
-
-
-            //}
-            if ((reader.HasRows) & (usernametxt.Text == "admin") & (passwordtxt.Text=="adminim")) //boyle sorun olursa belki databasede yetki verilebilir
-            {
+            string result = (string)command.ExecuteScalar();
+            string yetki = "select yetki from [kullanicilar] where kullanici = @username ";
+            SqlCommand command2 = new SqlCommand(yetki, connect);
+            command2.Parameters.AddWithValue("@username", usernametxt.Text);
+            int yetki_int = (int)command2.ExecuteScalar();
+            bool verified = Crypto.VerifyHashedPassword(result, passwordtxt.Text);
+            connect.Close();
+            
+            if ((verified == true) & (yetki_int == 1))
+            { 
                 Response.Redirect("adminpage.aspx");
                 Session["User"] = usernametxt.Text;
+            }
+            else if ((verified == true) & (yetki_int == 0))
+            { 
+
+                InvalidLoginLabel.Visible = true;
+                InvalidLoginLabel.Text = "Kullanıcının yetkisi yok.";
+                InvalidLoginLabel.ForeColor = System.Drawing.Color.Red;
             }
             else
             {
@@ -50,13 +49,6 @@ namespace saglik_treyleri.web1
                 InvalidLoginLabel.Text = "Kullanıcı adı veya şifre yanlış.";
                 InvalidLoginLabel.ForeColor = System.Drawing.Color.Red;
             }
-
-
         }
-
-
-
-
-
     }
 }
