9452820b3d59e0ddb71462688989b57bcc3f73d3
ecemces <113671363+ecemces@users.noreply.github.com>
2022-12-19 15:08:44 +0300

    Update login.aspx.cs

--- admin_web/login.aspx.cs
+++ admin_web/login.aspx.cs
@@ -9,46 +9,65 @@ using System.IO;
 using System.IO.Ports;
 using System.Web.Helpers;
 using System.Text;
+using Microsoft.Ajax.Utilities;
+
 namespace saglik_treyleri.web1
 {
     public partial class WebForm2 : System.Web.UI.Page
-    {        
-        SqlConnection connect = new SqlConnection("Data Source=localhost;Initial Catalog=SaglikTreyleri;Integrated Security=True");     
+    {
+        SqlConnection connect = new SqlConnection("Data Source=localhost;Initial Catalog=SaglikTreyleri;Integrated Security=True;MultipleActiveResultSets=true");
         protected void Page_Load(object sender, EventArgs e)
         {
+
         }
         protected void loginbutton_Click(object sender, EventArgs e)
-        {
+        {           
             connect.Open();
+            //şifre çekildi
             string password = "select sifre from [kullanicilar] where kullanici = @username ";
             SqlCommand command = new SqlCommand(password, connect);
             command.Parameters.AddWithValue("@username", usernametxt.Text);
             string result = (string)command.ExecuteScalar();
+            //yetki çekildi
             string yetki = "select yetki from [kullanicilar] where kullanici = @username ";
             SqlCommand command2 = new SqlCommand(yetki, connect);
             command2.Parameters.AddWithValue("@username", usernametxt.Text);
-            int yetki_int = (int)command2.ExecuteScalar();
-            bool verified = Crypto.VerifyHashedPassword(result, passwordtxt.Text);
-            connect.Close();
-            
-            if ((verified == true) & (yetki_int == 1))
-            { 
-                Response.Redirect("adminpage.aspx");
-                Session["User"] = usernametxt.Text;
-            }
-            else if ((verified == true) & (yetki_int == 0))
-            { 
+            //reader hazırlanıyor
+            string reader_icin= "select yetki from [kullanicilar] where kullanici = @username ";
+            SqlCommand reader_command = new SqlCommand(reader_icin, connect);
+            reader_command.Parameters.AddWithValue("@username", usernametxt.Text);
+            SqlDataReader dr =reader_command.ExecuteReader();
+            if (dr.HasRows)
+            {
+                int yetki_int= (int)command2.ExecuteScalar();
+                bool verified = BCrypt.Net.BCrypt.Verify(passwordtxt.Text, result);
+                if ((verified == true) & (yetki_int == 1))
+                {
+                    Response.Redirect("adminpage.aspx");
+                    Session["User"] = usernametxt.Text;
+                }
+                else if ((verified == true) & (yetki_int != 1))
+                { // şu an adminin ismi erz imiş gibi düşeneceğiz ama veritabanında yetki columnu eklenecek (yetki_table==1)
 
-                InvalidLoginLabel.Visible = true;
-                InvalidLoginLabel.Text = "Kullanıcının yetkisi yok.";
-                InvalidLoginLabel.ForeColor = System.Drawing.Color.Red;
-            }
+                    InvalidLoginLabel.Visible = true;
+                    InvalidLoginLabel.Text = "Kullanıcının yetkisi yok.";
+                    InvalidLoginLabel.ForeColor = System.Drawing.Color.Red;
+                }
+                else if (verified==false) {
+                    InvalidLoginLabel.Visible = true;
+                    InvalidLoginLabel.Text = "Kullanıcı adı veya şifre yanlış.";
+                    InvalidLoginLabel.ForeColor = System.Drawing.Color.Red;
+                }
+            }          
             else
             {
                 InvalidLoginLabel.Visible = true;
                 InvalidLoginLabel.Text = "Kullanıcı adı veya şifre yanlış.";
                 InvalidLoginLabel.ForeColor = System.Drawing.Color.Red;
             }
+            dr.Close();
+            connect.Close();
+
         }
     }
 }
