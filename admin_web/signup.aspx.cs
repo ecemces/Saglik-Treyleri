822a80785544f23569084c7136f8ead106149e3c
ecemces <113671363+ecemces@users.noreply.github.com>
2023-01-13 14:36:14 +0300

    Update signup.aspx.cs

--- admin_web/signup.aspx.cs
+++ admin_web/signup.aspx.cs
@@ -13,71 +13,78 @@ using static System.Net.Mime.MediaTypeNames;
 namespace saglik_treyleri.web1
 {
     public partial class WebForm3 : System.Web.UI.Page
-    {      
+    {
+       
         SqlConnection connect = new SqlConnection("Data Source=localhost;Initial Catalog=SaglikTreyleri;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            if (Session["User"] == null)
+            {
+                Response.Redirect("login.aspx");
+            }
         }
+
         protected void submitbutton_Click(object sender, EventArgs e)
         {
-            connect.Open();
-            string checkusername = "select * from [kullanicilar] where kullanici = @username ";
-            SqlCommand command = new SqlCommand(checkusername, connect);
-            command.Parameters.AddWithValue("@username", usernametxt.Text);
-            SqlDataReader reader = command.ExecuteReader();
+                connect.Open();
+                string checkusername = "select * from [kullanicilar] where kullanici = @username ";
+                SqlCommand command = new SqlCommand(checkusername, connect);
+                command.Parameters.AddWithValue("@username", usernametxt.Text);
+                SqlDataReader reader = command.ExecuteReader();
 
-            if (passwordtxt.Text=="" || usernametxt.Text == ""){
-                reader.Close();
-                InvalidLabel.Visible = true;
-                InvalidLabel.Text = "Kullanıcı adı ve şifre boş olamaz.";
-                InvalidLabel.ForeColor = System.Drawing.Color.Red;
-            }
-            else if (!reader.HasRows && passwordtxt0.Text==passwordtxt.Text)
-            {   
-                reader.Close();
-                var hash = BCrypt.Net.BCrypt.HashPassword(passwordtxt.Text);
-                string createUser = "insert into [kullanicilar]  (kullanici, sifre,yetki) values(@kullanici, @sifre,@yetki)";
-                SqlCommand addCommand = new SqlCommand(createUser, connect);
-                addCommand.Parameters.AddWithValue("@kullanici", usernametxt.Text);
-                addCommand.Parameters.AddWithValue("@sifre", hash);
-                if (CheckBox3.Checked)
+                if (passwordtxt.Text == "" || usernametxt.Text == "")
                 {
-                    addCommand.Parameters.AddWithValue("@yetki", 1);
+                    reader.Close();
+                    InvalidLabel.Visible = true;
+                    InvalidLabel.Text = "Kullanıcı adı ve şifre boş olamaz.";
+                    InvalidLabel.ForeColor = System.Drawing.Color.Red;
                 }
-                else if (!CheckBox3.Checked)
+                else if (!reader.HasRows && passwordtxt0.Text == passwordtxt.Text)
                 {
-                    addCommand.Parameters.AddWithValue("@yetki", 0);
-                }
-               
-                addCommand.ExecuteNonQuery();
-                connect.Close();
-                InvalidLabel.Visible = true;
-                InvalidLabel.Text = "Kullanıcı eklendi.";
-                InvalidLabel.ForeColor = System.Drawing.Color.Blue;
+                    reader.Close();
+                    var hash = BCrypt.Net.BCrypt.HashPassword(passwordtxt.Text);
+                    string createUser = "insert into [kullanicilar]  (kullanici, sifre,yetki) values(@kullanici, @sifre,@yetki)";
+                    SqlCommand addCommand = new SqlCommand(createUser, connect);
+                    addCommand.Parameters.AddWithValue("@kullanici", usernametxt.Text);
+                    addCommand.Parameters.AddWithValue("@sifre", hash);
+                    if (CheckBox3.Checked)
+                    {
+                        addCommand.Parameters.AddWithValue("@yetki", 1);
+                    }
+                    else if (!CheckBox3.Checked)
+                    {
+                        addCommand.Parameters.AddWithValue("@yetki", 0);
+                    }
 
-                //Response.Redirect("adminpage.aspx");
-            }
-            else if (reader.HasRows)
-                
-            {
-                reader.Close();
-                InvalidLabel.Visible = true;
-                InvalidLabel.Text = "Bu kullanıcı adı kullanılmaktadır.";
-                InvalidLabel.ForeColor = System.Drawing.Color.Red;
-            }
-            else if (!reader.HasRows && passwordtxt0.Text != passwordtxt.Text)
-                
-            {
-                reader.Close();
-                InvalidLabel.Visible = true;
-                InvalidLabel.Text = "Lütfen şifrenizi doğrulayın.";
-                InvalidLabel.ForeColor = System.Drawing.Color.Red;
-            }
+                    addCommand.ExecuteNonQuery();
+                    connect.Close();
+                    InvalidLabel.Visible = true;
+                    InvalidLabel.Text = "Kullanıcı eklendi.";
+                    InvalidLabel.ForeColor = System.Drawing.Color.Blue;
+
+                    //Response.Redirect("adminpage.aspx");
+                }
+                else if (reader.HasRows)
+
+                {
+                    reader.Close();
+                    InvalidLabel.Visible = true;
+                    InvalidLabel.Text = "Bu kullanıcı adı kullanılmaktadır.";
+                    InvalidLabel.ForeColor = System.Drawing.Color.Red;
+                }
+                else if (!reader.HasRows && passwordtxt0.Text != passwordtxt.Text)
+
+                {
+                    reader.Close();
+                    InvalidLabel.Visible = true;
+                    InvalidLabel.Text = "Lütfen şifrenizi doğrulayın.";
+                    InvalidLabel.ForeColor = System.Drawing.Color.Red;
+                }
+            
         }
-
         protected void usernametxt_TextChanged(object sender, EventArgs e)
         {
+
         }
 
         protected void logoutbutton_Click(object sender, EventArgs e)
@@ -87,6 +94,7 @@ namespace saglik_treyleri.web1
 
         protected void CheckBox3_CheckedChanged(object sender, EventArgs e)
         {
+
         }
     }
 }
